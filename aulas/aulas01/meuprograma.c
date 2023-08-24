5ea7c2ade084f8f5810b2fb5e1459857a31c753b
ClaubertyVinici <24024164-ClaubertyVinici@users.noreply.replit.com>
2023-08-24 00:46:11 +0000

    arquivos da aula 01

---
+++ aulas/aulas01/meuprograma.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+  int a1,
+
+  printf("ola mundo\n");
+  printf("a raiz quadrada de 2 é %f\n", sqrt(2));
+  
+  return 0;
+}
---
+++ praticas/pratica01/questao01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+float nota_a1 = 5.5f;
+  float nota_a2 = 4.8f;
+  
+float media = 0.4 * nota_a1 + 0.6 * nota_a2;
+  printf("a media é %f", media);
+  
+  return 0;
+}
